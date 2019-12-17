cc3eba4734f4202eeef2e8fc3b9d06d69d1caaaa
miia <miia@com.tw>
2019-12-17 16:21:37 +0800

    糸娟女子酉鬼

--- app/src/main/java/com/tom/game/MainActivity.java
+++ app/src/main/java/com/tom/game/MainActivity.java
@@ -17,25 +17,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void findView() {
-        findViewById(R.id.button_down).setOnClickListener(this);
-        findViewById(R.id.button_up).setOnClickListener(this);
-        findViewById(R.id.button_right).setOnClickListener(this);
-        findViewById(R.id.button_left).setOnClickListener(this);
+        findViewById(R.id.arrow_down).setOnClickListener(this);
+        findViewById(R.id.arrow_left).setOnClickListener(this);
+        findViewById(R.id.arrow_right).setOnClickListener(this);
+        findViewById(R.id.arrow_up).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.button_up:
+            case R.id.arrow_up:
                 Log.d("MainActivity", "onClick_UP");
                 break;
-            case R.id.button_down:
+            case R.id.arrow_down:
                 Log.d("MainActivity", "onClick_DOWN");
                 break;
-            case R.id.button_right:
+            case R.id.arrow_right:
                 Log.d("MainActivity", "onClick_RIGHT");
                 break;
-            case R.id.button_left:
+            case R.id.arrow_left:
                 Log.d("MainActivity", "onClick_LEFT");
                 break;
         //為了區別是按了哪個button，所以用getId去得到每個button的id
